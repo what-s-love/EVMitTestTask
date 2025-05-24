82de5b0f08dcfb3ed6f57922a18d92f7a930dc37
VladislavK <barabanabanan@gmail.com>
2025-05-24 18:06:44 +0400

    Initial commit

---
+++ src/main/java/ge/evmittesttask/EvMitTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ge.evmittesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvMitTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvMitTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/evmittesttask/EvMitTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.evmittesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvMitTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
